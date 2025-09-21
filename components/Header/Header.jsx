1c95262d387996d7cf50c10386f22334f4108a17
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-21 23:35:08 +0500

    proect

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
-const Navbar = () => {
+
+const Header = () => {
   return (
-    <div>
-         App
+    <div className="header">
+      <div className="container">
+        <div className="header-container">
+           <Link href="/" ><Image src="/logo.svg" alt="Logo" width={120} height={40} /></Link>
+              <ul className="header-list">
+                    <li className="header-item"><Link href={} ></Link></li>
+              </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Header;
